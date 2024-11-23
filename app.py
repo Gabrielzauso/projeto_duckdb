c65f239206a69e9159b4ff50587dfc367b902076
Gabriel Araujo <gapaes4@gmail.com>
2024-11-23 02:05:35 -0300

    version final

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
+from pipeline_0 import pipeline
 
+st.title('Processador de Arquivos')
 
-def main():
-    st.write("Hello world oi ")
-
-if __name__ == "__main__":
-    main()
+if st.button('Processar'):
+    with st.spinner('Processando...'):
+        logs = pipeline()
+        # Exibe os logs no Streamlit
+        for log in logs:
+            st.write(log)
